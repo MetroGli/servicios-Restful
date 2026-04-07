070fd6183b7d3f72c4ec82bee4a8f3527cd4e071
Duseb <duvan12310@outlook.com>
2026-04-07 03:14:16 -0500

    Refactor Person class and update XML annotations

--- src/main/java/co/edu/unipiloto/ws/testws/model/Person.java
+++ src/main/java/co/edu/unipiloto/ws/testws/model/Person.java
@@ -1,28 +1,60 @@
-package co.edu.unipiloto.ws.testws.model;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidad;
 
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
-@XmlRootElement
+/**
+ *
+ * @author duvan
+ */
+@XmlRootElement(name = "persona")
+@XmlType (propOrder= {"id","fullName","age", "salario"})
 public class Person {
-
     private int id;
     private String fullName;
     private int age;
-
-    private static final double SALARIO_MINIMO = 1300000;
-
-    public Person() {}
-
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-
-    public String getFullName() { return fullName; }
-    public void setFullName(String fullName) { this.fullName = fullName; }
-
-    public int getAge() { return age; }
-    public void setAge(int age) { this.age = age; }
-
-    public double getSalario() {
-        return (age * SALARIO_MINIMO) / 3;
+    private double salario;
+    
+    public Person (){
+    
     }
+@XmlElement
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+@XmlElement
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+@XmlElement
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+@XmlElement
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    
 }
