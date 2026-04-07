1f01a7970058034bb5a54abbdeebc14a63108076
Duseb <duvan12310@outlook.com>
2026-04-07 03:13:51 -0500

    Refactor Service class and update endpoint methods

--- src/main/java/co/edu/unipiloto/ws/testws/services/Service.java
+++ src/main/java/co/edu/unipiloto/ws/testws/services/Service.java
@@ -1,22 +1,33 @@
-package co.edu.unipiloto.ws.testws.services;
+package servicios;
 
-import co.edu.unipiloto.ws.testws.model.Person;
+import entidad.Person;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 import java.util.*;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
-@Path("/person")
+/**
+ *
+ * @author duvan
+ */
+@Path("service")
 public class Service {
 
     private static Map<Integer, Person> persons = new HashMap<Integer, Person>();
+    private static final double SALARIO_MINIMO = 1300000;
 
     static {
         for (int i = 0; i < 10; i++) {
             Person person = new Person();
             int id = i + 1;
             person.setId(id);
-            person.setFullName("My wonderful Person " + id);
+            person.setFullName("My wonderfull Person " + id);
             person.setAge(new Random().nextInt(40) + 1);
+            person.setSalario(person.getAge() * SALARIO_MINIMO / 3);
             persons.put(id, person);
         }
     }
@@ -61,22 +72,30 @@ public class Service {
     @GET
     @Path("/promedio")
     @Produces(MediaType.APPLICATION_XML)
-    public double promedioSalarios() {
+    public String getPromedioSalario() {
+
         double suma = 0;
+
         for (Person p : persons.values()) {
             suma += p.getSalario();
         }
-        return suma / persons.size();
+
+        double promedio = suma / persons.size();
+
+        return "<promedio>" + promedio + "</promedio>";
     }
 
     @GET
     @Path("/suma")
     @Produces(MediaType.APPLICATION_JSON)
-    public double sumaSalarios() {
+    public String getSumaSalarios() {
+
         double suma = 0;
+
         for (Person p : persons.values()) {
             suma += p.getSalario();
         }
-        return suma;
+
+        return "{\"suma\":" + suma + "}";
     }
 }
